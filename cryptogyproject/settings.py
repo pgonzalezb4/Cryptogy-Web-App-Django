94f8abb8d3e0d55fdb974f1274c43e38eccd2667
pgonzalezb4 <pablo.gonzalez7554@gmail.com>
2022-04-03 15:26:30 -0500

    Cambios menores.

--- cryptogyproject/settings.py
+++ cryptogyproject/settings.py
@@ -15,8 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, "static"),
 )
+STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
 MEDIA_URL = '/media/'
 
@@ -122,4 +123,3 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
