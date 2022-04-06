08d1e28fe785369baa5d5791309b92f54273f3ec
pgonzalezb4 <pablo.gonzalez7554@gmail.com>
2022-04-06 12:50:05 -0500

    Avance implementación Rabin

--- cryptogyapp/views.py
+++ cryptogyapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 
 from .models import Cryptosystem
-from .forms import RsaForm
+from .forms import RsaForm, RabinForm
 
 # Create your views here.
 def index(request):
@@ -51,6 +51,33 @@ def rsaView(request):
     return HttpResponse(template.render(context, request))
 
 def rabinView(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        print("POST request.")
+        # create a form instance and populate it with data from the request:
+        form = RabinForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            cd = form.cleaned_data
+            p = cd['primeP']
+            q = cd['primeQ']
+            cleartext = cd['clearText']
+            ciphertext = cd['cipherText']
+            print(cd)
+            # Encriptacion Rabin
+            if 'encrypt' in request.POST:
+                pass # Codigo para encriptar...
+            # Desencriptacion Rabin
+            elif 'decrypt' in request.POST:
+                pass # Codigo para desencriptar...
+            return HttpResponse('/Rabin/')
+
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = RabinForm()
+
     thisCryptosystem = Cryptosystem.objects.get(name="Rabin")
     template = loader.get_template('cryptogyapp/rabin.html')
     context = {
