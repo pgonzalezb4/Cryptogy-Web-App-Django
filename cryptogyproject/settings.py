0a28265ccc9b1fbbf7ca06d3d402dd214a5d233f
pgonzalezb4 <pablo.gonzalez7554@gmail.com>
2022-04-07 08:39:41 -0500

    Update settings.py

--- cryptogyproject/settings.py
+++ cryptogyproject/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
