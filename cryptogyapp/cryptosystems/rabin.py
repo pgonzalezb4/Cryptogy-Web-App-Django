80fa086b945ffbc8b2e6bdf3032e9061ae6af4fe
pgonzalezb4 <pablo.gonzalez7554@gmail.com>
2022-04-08 18:08:35 -0500

    Implementación Rabin

--- cryptogyapp/cryptosystems/rabin.py
+++ cryptogyapp/cryptosystems/rabin.py
@@ -1,162 +1,55 @@
-import binascii
-import random
+# 105064506 鄭柏偉 HW3
+# Rabin Public-Key Cryptosystem
+
+import prime
+import rabinutils
+
+def delete_space(string):
+    lst = string.split(' ')
+    output = ''
+    for i in lst:
+        output += i
+    return output
 
 
-def isPrime(mrc):
-    """Run 20 iterations of Rabin Miller Primality test"""
-    maxDivisionsByTwo = 0
-    ec = mrc - 1
-    while ec % 2 == 0:
-        ec >>= 1
-        maxDivisionsByTwo += 1
-    assert 2**maxDivisionsByTwo * ec == mrc - 1
-
-    def trialComposite(round_tester):
-        if pow(round_tester, ec, mrc) == 1:
-            return False
-        for i in range(maxDivisionsByTwo):
-            if pow(round_tester, 2**i * ec, mrc) == mrc - 1:
-                return False
-        return True
-
-    # Set number of trials here
-    numberOfRabinTrials = 20
-    for i in range(numberOfRabinTrials):
-        round_tester = random.randrange(2, mrc)
-        if trialComposite(round_tester):
-            return False
-    return True
+def add_space(string):
+    string = string[::-1]
+    string = ' '.join(string[i:i + 8] for i in range(0, len(string), 8))
+    return string[::-1]
 
 
-class Key:
-    def __init__(self, n, value):
-        self.n = n
-        self.value = value
+# main()
+if __name__ == '__main__':
 
-    def __repr__(self) -> str:
-        return "Key(%i, %i)" % (self.n, self.value)
+    ###########################################################
+    # 256-bit prime number generation
+    print('<Miller-Rabin>')
+    print(add_space(format(prime.generate_a_prime_number(256), 'x')))
 
+    ###########################################################
+    # Encryption
+    print('\n<Rabin Encryption>')
 
-def gcd(p, q):
-    while q != 0:
-        (p, q) = (q, p % q)
-    return p
+    # p and q has 128 bits
+    p = int(delete_space(input('p = ')))   # p = daaefe652cad1614f17e87f2cd80973f (290680322482407050642711539016771082047)
+    q = int(delete_space(input('q = ')))   # q = f99988626723eef2a54ed484dfa735c7 (331774958573786300819827599586292610503)
+    n = p*q
+    print('n = pq =', n)
 
-def egcd(a, b):
-    if a == 0:
-        return (b, 0, 1)
-    else:
-        g, y, x = egcd(b % a, a)
-        return (g, x - (b // a) * y, y)
+    # plaintext has 224 bits
+    # Rabin encryption
+    inp = input('Plaintext = ')
+    plaintext = int.from_bytes(inp.encode(), 'big') # plaintext = be000badbebadbadbad00debdeadfacedeafbeefadd00addbed00bed (20009354183959636439822187081543763705494133470478467066835281906669)
+    ciphertext = rabinutils.encryption(plaintext, n)
+    print('Ciphertext =', ciphertext)
 
-def modinv(a, m):
-    g, x, y = egcd(a, m)
-    if g != 1:
-        raise Exception('modular inverse does not exist')
-    else:
-        return x % m
-
-def gen_keys(p, q):
-    if not isPrime(p) or not isPrime(q):
-        return False
-
-    n = p * q
-
-    l = (p - 1) * (q - 1) // gcd(p - 1, q - 1)
-
-    e = 1
-    for i in range(l - 1, 1, -1):
-        if gcd(i, l) == 1:
-            e = i
-            break
-
-    # if 1 >= e or e >= l or gcd(e, l) != 1:
-    #    return False
-
-    #d = pow(e, -1, l)
-    d = modinv(e, l)
-
-    return Key(n, e), Key(n, d)
-
-
-def string_to_4list(text):
-    list_of_messages = list()
-    pos = 0
-    while pos < len(text):
-        try:
-            list_of_messages.append(text[pos : pos + 4])
-            pos += 4
-        except:
-            list_of_messages.append(text[pos : len(text)])
-    return list_of_messages
-
-
-def string2int(text):
-    bits_ = text_to_bits(text)
-    return int(bits_, 2)
-
-
-def text_to_bits(text, encoding="utf-8", errors="surrogatepass"):
-    bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
-    return bits.zfill(8 * ((len(bits) + 7) // 8))
-
-
-def int2bytes(i):
-    hex_string = "%x" % i
-    n = len(hex_string)
-    return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
-
-
-def int2string(i, encoding="utf-8", errors="surrogatepass"):
-    print('Input', i)
-    bytes_ = int2bytes(i)
-    print('Bytes', bytes_)
-    return bytes_.decode(encoding, errors)
-
-
-def encrypt(clear_text, public_key):
-    n, value = public_key.n, public_key.value
-
-    ciphertext = list()
-    list_of_messages = string_to_4list(clear_text)
-
-    for subtext in list_of_messages:
-
-        m = string2int(subtext)
-        c = pow(m, value, n)
-        ciphertext.append(c)
-
-    return ciphertext
-
-
-def decrypt(cipher_text, private_key):
-    n, value = private_key.n, private_key.value
-
-    cleartext = list()
-
-    for c in cipher_text:
-        m = pow(c, value, n)
-        cleartext.append(int2string(m))
-
-    return "".join(cleartext)
-
-if __name__ == "__main__":
-
-    p = 8258171947
-    q = 5891608337
-
-    pub, priv = gen_keys(p, q)
-    print(pub, priv)
-    print(
-        encrypt(
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-            pub,
-        )
-    )
-    print(
-        decrypt(
-        encrypt(
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-            pub,
-        ), priv)
-    )
+    ###########################################################
+    # Decryption
+    print('\n<Rabin Decryption>')
+    ciphertext = int(delete_space(input('Ciphertext = ')))    # 0x5452361adb4c34be04a5903ae00793bc1086e887ebed06e23ffba0b4a4348cc0
+    print('Private Keys :')
+    p = int(delete_space(input('p = ')))  # 0xd5e68b2b5855059ad1a80dd6c5dc03eb
+    q = int(delete_space(input('q = ')))  # 0xc96c6afc57ce0f53396d3b32049fe2d3
+    plaintext = int(format(rabinutils.decryption(ciphertext, p, q), 'x').zfill(226 // 4), 16)
+    plaintext = plaintext.to_bytes((n.bit_length() + 7) // 8, 'big').decode()
+    print('Plaintext =', plaintext)
